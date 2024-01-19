49978672f9c603acec079809302f66dfc953dd6b
jake-knox- <jakeknox11@hotmail.co.uk>
2024-01-19 13:11:30 +0000

    routing/nav and basic file setup for pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+// <Switch> component has been deprecated and replaced with the <Routes> component.
 
-
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
+import Register from './components/Register';
 
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="app"> {/* Apply the styles to the entire App component */}
+        <Navbar />
 
-    </div>
+        <hr />
+
+        <Routes>
+
+          <Route exact path="/" Component={Home}></Route>
+          <Route exact path="/about" Component={About}></Route>
+          <Route exact path="/dashboard" Component={Dashboard}></Route>
+          <Route exact path="/register" Component={Register}></Route>
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
